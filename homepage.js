53adf183ea11035d398942d304c9341019aba8b4
Alexander Terry <alterry@buffalo.edu>
2024-06-30 19:59:02 -0400

    Page is almost complete, Adjusted most of the margins, need to change review cards design still

---
+++ homepage.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const reviewsContainer = document.querySelector(".reviews");
+    if (reviewsContainer) {
+        reviewsContainer.scrollLeft = 0; // Scrolls to the start
+    }
+});
