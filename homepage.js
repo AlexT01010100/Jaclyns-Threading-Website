bf80a550eb2934d3bde53894f0d54002db424fb6
Alexander Terry <alterry@buffalo.edu>
2024-06-30 23:09:31 -0400

    Page is essentially finished

--- homepage.js
+++ homepage.js
@@ -9,3 +9,40 @@ document.addEventListener("DOMContentLoaded", function () {
         reviewsContainer.scrollLeft = middleScroll;
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const images = document.querySelectorAll(".sliding-image-container img");
+
+    let currentIndex = 0;
+    const intervalTime = 3000; // Time between slides in milliseconds
+    let slideInterval;
+
+    const nextSlide = () => {
+        currentIndex = currentIndex < images.length - 1 ? currentIndex + 1 : 0;
+        scrollToImage(currentIndex);
+    };
+
+    const scrollToImage = (index) => {
+        const container = document.querySelector(".sliding-image-container");
+        const image = images[index];
+        container.scroll({
+            left: image.offsetLeft,
+            behavior: 'smooth'
+        });
+    };
+
+    // Auto slide
+    if (images.length > 1) {
+        slideInterval = setInterval(nextSlide, intervalTime);
+    }
+
+    // Allow clicking on images to scroll to them
+    images.forEach((img, index) => {
+        img.addEventListener('click', () => {
+            currentIndex = index;
+            scrollToImage(currentIndex);
+            clearInterval(slideInterval);
+            slideInterval = setInterval(nextSlide, intervalTime);
+        });
+    });
+});
